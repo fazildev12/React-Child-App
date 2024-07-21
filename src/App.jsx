06c7f14cdc308c8bd01c222414699f54834740fc
fazildev12 <fazilfazillisaf17@gmail.com>
2024-07-22 00:40:19 +0530

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,9 @@ import ReactDOM from "react-dom/client";
 import  Counter  from "./Counter";
 
 const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
+  <div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, sit in. Distinctio molestias reprehenderit quas vel earum, veritatis obcaecati inventore dignissimos quis adipisci?</p>
     <Counter />
-    <button type="button" class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Red</button>
-    <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900">Red</button>
   </div>
 );
 const rootElement = document.getElementById("app")
