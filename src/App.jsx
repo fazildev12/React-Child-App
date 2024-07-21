c47d147e42f4c80f19904fede01ca30b54eb12e6
fazildev12 <fazilfazillisaf17@gmail.com>
2024-07-21 23:46:35 +0530

    created counter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import "./index.scss";
+import  Counter  from "./Counter";
 
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: react-child-app</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div>
+    <Counter />
   </div>
 );
 const rootElement = document.getElementById("app")
